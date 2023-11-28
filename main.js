7a3bf7cccd4246e92adc12e8f905f09cb566add5
Your Name <gms441@mail.ru>
2023-11-28 22:20:47 +0700

    Вернулся к старому плееру, обновил дизайн

--- main.js
+++ main.js
@@ -1,25 +1,47 @@
-const mainContainer = document.querySelector('.main-container');
 const audio = document.querySelectorAll('.audio-player-control');
 const playerControls = Array.apply(null, audio);
 const radioStationLabels = document.querySelectorAll('.radio-station-label');
 const mobileControls = document.querySelectorAll('.mobile-control');
 
+// одновременно может играть только один плеер
+playerControls.forEach(control => {
+    control.addEventListener('play', e => {
+        const playerId = e.target.id;
+        const radioType = playerId.split('-')[2];
+        document.title = `${radioType} radio`;
+        playerControls.forEach(player => {
+            player.id !== playerId && player.pause()
+        })
+    })
+})
 
-/** Работа с плеером youtube */
-// lofi-player-yt
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+// лейбл плеера получает возможности play\pause
+radioStationLabels.forEach(label => {
+    label.addEventListener('click', e => {
+        let playerController = e.target.parentElement.lastElementChild;
+        playerController.paused ? playerController.play() : playerController.pause();
+    })
+})
 
-var player;
-function onYouTubePlayerAPIReady() {
-    player = new YT.Player('lofi-player-yt', {
-        height: '20',
-        width: '20',
-        videoId: 'jfKfPfyJRdk'
-    });
-}
+// кнопки управления плеером для мобильнйо версии TODO вынести в js для смартфонов
+mobileControls.forEach(control => {
+    control.addEventListener('click', e => {
+        const button = e.currentTarget;
+        const playerAudioController = button.nextElementSibling;
 
-document.getElementById('start-btn').addEventListener('click', () => player.playVideo(), false)
-document.getElementById('stop-btn').addEventListener('click', () => player.stopVideo(), false)
+        console.log(button)
+
+        playerAudioController.paused ? playerAudioController.play() : playerAudioController.pause();
+
+        // меняем у текущего
+        button.classList.toggle('pause');
+        button.classList.toggle('play');
+
+        mobileControls.forEach(controller => {
+            if(controller.nextElementSibling.id !== playerAudioController.id) {
+                controller.classList.add('play');
+                controller.classList.remove('pause');
+            }
+        })
+    })
+})
